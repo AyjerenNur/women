fa891a9190ef20adbb921c366ce09523c835c7e5
Gulmirabdy <gabdyrahmanova90@gmail.com>
2023-05-26 19:45:01 +0500

    #5

--- lib/colors.dart
+++ lib/colors.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 Color pink = Color(0xFFEED0D0);
+Color darkpink = Color(0xFFEF8F8F);
 Color textcolor = Color(0xff313536);
 Color textcolor2 = Color(0xff3B3C3E);
 Color textfieldcolor = Color(0xffF2F2F2);
@@ -8,5 +9,6 @@ Color textfieldcolor2 = Color(0xff777778);
 Color textcolor3 = Color(0xffAAAAAA);
 Color textcolor4 = Color(0xff000000);
 Color black = Color(0xff000000);
+Color white = Color(0xffffffff);
 Color lightgray = Color(0xff868889);
 
